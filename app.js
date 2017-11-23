c6f2a39a235803a4232830196ddb8fd6d86cfa62
SombreroElGringo <florent.pailhes@hotmail.fr>
2017-11-23 14:37:34 +0100

    App enhancement

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const chalk = require('chalk');
 
+const app = express();
 const PORT = 8081;
 
 
@@ -18,8 +20,30 @@ app.get("*", (req, res, next) => {
 		'"></body>')
 });
 
-app.listen(PORT, function (err) {
-	if (err)
-		throw err;
-	console.log('Listening on http://localhost:' + PORT);
-});
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_DOCKER_URI || 'mongodb://localhost:27017/test-unitaire',
+                {
+                    useMongoClient: true
+				})
+				.then(() => {
+
+					console.log('%s Connection has been established successfully with the database', chalk.green('✓'));
+					
+					/**
+					 * Running server
+					 */
+					app.listen(PORT, (err) => {
+						if (err)
+						console.error(err);
+						console.log('%s App is running at http://localhost:%d', chalk.green('✓'), PORT);
+						console.log('-- Press CTRL-C to stop --\n');
+					});
+				})
+				.catch(err => {
+					console.error(err);
+					console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+					process.exit();
+				});
