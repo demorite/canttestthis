d62c9bee3a9551417252626339be71570caa3cd4
SombreroElGringo <florent.pailhes@hotmail.fr>
2017-11-23 14:21:19 +0100

    Models implemented

---
+++ models/Article.js
@@ -0,0 +1,17 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const articleSchema = new  mongoose.Schema({
+    
+    name: String,
+    items: Number,
+    flag: {
+        type: String,
+        enum: ['OK', 'NOT OK']
+    }
+
+}, { timestamps: true });
+
+const Article = mongoose.model('articles', articleSchema);
+
+module.exports = Article;
